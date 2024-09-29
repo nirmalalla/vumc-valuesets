4362afce7cf7ab8d3bdc390176b857046001a446
nirmalalla <allanirmal@gmail.com>
2024-09-28 21:06:08 -0500

    fixing speed issue

--- server/app.py
+++ server/app.py
@@ -3,38 +3,69 @@ from data import searchMedName
 from data import associatedGroupers
 from data import probsAssociatedGroupers
 from data import grabGroupersMeds
+from data import grabGrouperProblems
 from data import searchProblem
+from data import grabGroupers
+from data import grabGroupersTxt
+from data import grabMeds
+from data import grabProbs
+from data import grabGroupersMeds
+
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+groupersTxt = None
+groupersJson = None
+problems = None
+meds = None
+grouperMeds = None
+
+
+def run_on_startup():
+    global groupersTxt, groupersJson, problems, meds, grouperMeds
+
+    groupersTxt = grabGroupersTxt()
+    groupersJson = grabGroupers()
+    problems = grabProbs()
+    meds = grabMeds()
+    grouperMeds = grabGroupersMeds()
+
+run_on_startup()
+
 @app.route("/medname", methods=["GET"])
 def searchByMedname():
     headers = request.headers
     medname = headers.get("medname")
-    return jsonify(searchMedName(medname))
+    return jsonify(searchMedName(medname, meds))
 
 @app.route("/grouper", methods=["GET"])
 def getGroupers():
     headers = request.headers
     med_id = headers.get("medication_id")
-    return jsonify(associatedGroupers(med_id))
+    return jsonify(associatedGroupers(med_id, groupersJson))
 
 @app.route("/groupermeds", methods=["GET"])
 def grabGrouperMeds():
     headers = request.headers
     name = headers.get("grouper_name")
-    return jsonify(grabGroupersMeds(name))
+    return jsonify(grouperMeds[name])
 
 @app.route("/problemgroupers", methods=["GET"])
 def getProbGroupers():
     headers = request.headers
     dx_id = headers.get("dx_id")
-    return jsonify(probsAssociatedGroupers(dx_id))
+    return jsonify(probsAssociatedGroupers(dx_id, groupersTxt))
 
 @app.route("/problems", methods=["GET"])
 def getProbs():
     headers = request.headers
     name = headers.get("problem_name")
-    return jsonify(searchProblem(name))
+    return jsonify(searchProblem(name, problems))
+
+@app.route("/grouperproblems", methods=["GET"])
+def getGrouperProbs():
+    headers = request.headers
+    name = headers.get("name")
+    return jsonify(grabGrouperProblems(name, problems, groupersTxt))
--- server/data.py
+++ server/data.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-
+import numpy as np
 def grabMeds():
     try:
         with open("data/meds.json", "r") as file:
@@ -32,7 +32,7 @@ def grabGroupersTxt():
     df = df.to_dict(orient="records")
     return df
 
-def grabGroupersMeds(name):
+def grabGroupersMeds():
     try:
         with open("data/grouperMeds.json", "r") as file:
             data = json.load(file)
@@ -41,26 +41,43 @@ def grabGroupersMeds(name):
     except json.JSONDecodeError:
         print("error loading json")
 
-    return data[name]
+    return data
         
 
-def searchMedName(name):
-    meds = grabMeds()
+def searchMedName(name, meds):
     filtered_data = list(filter(lambda med: name.upper() in med["medname"], meds))
     filtered_data = sorted(filtered_data, key= lambda med: med["patients"], reverse=True)
     return filtered_data
 
-def associatedGroupers(med_id):
-    groupers = grabGroupers()
-    filtered_data = list(filter(lambda grouper: "erx" in grouper and med_id == grouper["erx"], groupers))
+def associatedGroupers(med_id, groupers):
+    filtered_data = list(filter(lambda grouper: "erx" in grouper and str(med_id) == str(grouper["erx"]), groupers))
     return filtered_data
 
-def probsAssociatedGroupers(dx_id):
-    groupers = grabGroupersTxt()
-    groupers = list(filter(lambda grouper: grouper["edg"] == dx_id, groupers))
-    return groupers
+def probsAssociatedGroupers(dx_id, groupers):
+    filtered_groupers = []
 
-def searchProblem(name):
-    probs = grabProbs()
+    for grouper in groupers:
+        if (str(grouper["edg"]) == dx_id):
+            filtered_groupers.append(grouper)
+    
+    return filtered_groupers
+
+def searchProblem(name, probs):
     filtered_data = list(filter(lambda problem: "dx_name" in problem and name in str(problem["dx_name"]), probs))
     return filtered_data
+
+def grabGrouperProblems(name, problems, groupers):
+    edgs = []
+
+    for grouper in groupers:
+        if "grouper_name" in grouper and grouper["grouper_name"] == name:
+           edgs.append(str(grouper["edg"]))
+    
+    associatedProblems = []
+    
+    for problem in problems:
+        if "dx_id" in problem and np.isnan(problem["dx_id"]) == False and str(int(problem["dx_id"])) in edgs:
+            associatedProblems.append(problem)
+
+    return associatedProblems 
+
